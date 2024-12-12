da55de60ff18dc26063789dbb83d565b3e1cdb9b
ArjayanM <arjayan.moustafa@rwth-aachen.de>
2024-12-12 14:22:58 +0100

    Umbennen in Excel

--- AnalyseScript.R
+++ AnalyseScript.R
@@ -19,19 +19,6 @@ raw.short <- raw[,c(-1,-2,-4,-8:-18,-23,-26:-28,-38:-49,-56:-77,-98:-111)]
 
 # Variablen umbenennen ----
 
-dput(names(raw.short))
-
-names(raw.short) <- c("Status", "Progress", "Duration (in seconds)", "Finished", "Gender", "Age", "Bildungsabschluss", "Wohnort", 
-                      "big5_1", "big5_2n", 
-                      "ati_1", "ati_2", "ati_3n", "ati_4", "ati_5", "ati_6n", "ati_7", "ati_8n", "ati_9", 
-                      "behörden_1", "behörden_2", "behörden_3", "behörden_4", "behörden_5", "behörden_6n", 
-                      "sz_B_1", "sz_B_2", "sz_B_3", "sz_B_4", "sz_B_5", "sz_B_6", 
-                      "privat_B_1n", "privat_B_2n", "privat_B_3n", "privat_B_4", "privat_B_5", 
-                      "vertrauen_B_1", "vertrauen_B_2", 
-                      "bedienenb_B_1", "bedienenb_B_2", "bedienenb_B_3", "bedienenb_B_4", "bedienenb_B_5", 
-                      "zeit_B_1", "zeit_B_2", 
-                      "FL_15_DO_SzenarioA", "FL_15_DO_SzenarioB", "FL_15_DO_SzenarioC")
-
 generate_codebook(raw.short, "data/Methodenseminar+WS2425_6.+Dezember+2024_13.12.csv", "data/codebook.csv")
 codebook <- read_codebook("data/codebook_final.csv")
 names(raw.short) <- codebook$variable
